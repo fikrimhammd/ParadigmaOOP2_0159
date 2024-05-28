2e3a4159d05222b382bc447dea14b92669f372f7
fikrimhammd <fikri.kdi60@gmail.com>
2024-05-28 12:03:30 +0700

    class lia

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -22,3 +22,33 @@ public:
 	} 
 };
 
+class lia : public seseorang
+{
+public:
+	void pesan()
+	{
+		cout << "pesan dari lia" << endl;
+	}
+};
+
+int main()
+{
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+
+	//akses method pesan() dari class seseorang
+	//   a. seseorang::pesan();
+
+	return 0;
+}
+
+
+
+
+
